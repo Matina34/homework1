7ad4fabc8633ccc052ba3c267b8da2af960e43a4
Matina Ravan <matina@Matinas-MacBook-Air.local>
2025-09-27 20:32:44 +0200

    update

--- part2.py
+++ part2.py
@@ -14,9 +14,11 @@ r =0.04
 
 #loop that runs until the current savings are greater than or equal to the portion of the down payment
 while portion_down_payment > current_savings:
-    current_savings += current_savings*r/12  
+
+    current_savings += current_savings*r/12 
     current_savings += portion_saved*(annual_salary/12)
     month +=1
+
 # Apply semi-annual raise every 6 months
     if month%6==0:
         annual_salary += annual_salary*semi_annual_raise
