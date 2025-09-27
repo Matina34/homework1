9202956ae73b956d06622d3dd3e9f1a83e2ecce8
Matina Ravan <matina@Matinas-MacBook-Air.local>
2025-09-27 19:41:42 +0200

    update

--- part2.py
+++ part2.py
@@ -4,7 +4,8 @@
 annual_salary =float(input("Enter your annual salary in Lyon: "))
 portion_saved=float(input("Enter the portion of salary to be saved as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home in Lyon:  "))
-semi_annual= float(input("Enter the sami annual saraly raise: "))
+semi_annual_raise= float(input("Enter the sami annual saraly raise: "))
+
 portion_down_payment = total_cost*0.25
 current_savings=0
 month=0
@@ -16,6 +17,9 @@ while portion_down_payment > current_savings:
     current_savings += current_savings*r/12  
     current_savings += portion_saved*(annual_salary/12)
     month +=1
+    if month%6==0:
+        annual_salary += annual_salary*semi_annual_raise
+
 
         
 
